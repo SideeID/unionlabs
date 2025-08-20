89fad2ab0ef9165dd86f47e46c7259f6acf0ecb0
SideeID <sandipratama164026@gmail.com>
2025-08-20 14:16:37 +0700

    Refactor code structure for improved readability and maintainability

---
+++ resources/views/test.blade.php
@@ -0,0 +1,27 @@
+<x-home-layout>
+    <div class="relative">
+        <x-home.hero-section
+            search-placeholder="Cari produk vape..."
+            :cart-count="12"
+            :images="['/assets/Background.png','/assets/Background2.png']"
+        />
+        <div class="absolute left-1/2 -translate-x-1/2 top-[80%] w-full px-4">
+            <x-home.location-section />
+        </div>
+    </div>
+    <x-home.category-grid-section />
+    <x-home.voucher-banner-section
+        voucher1Title="Promo Spesial"
+        voucher1Subtitle="Hanya hari ini!"
+        voucher1Button="Ambil Sekarang"
+        voucher2Title1="Mau Voucher"
+        voucher2Title2="Diskon 20RB?"
+    />
+    <x-home.special-section />
+    <x-home.official-merch-section />
+    <x-home.product-section title="Produk Terbaru" />
+    <x-home.product-section title="Produk Terlaris" />
+    <x-home.promo-section />
+    <x-home.chat-admin-section />
+
+</x-home-layout>
